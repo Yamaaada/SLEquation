a1fe18f37ba44a95157fc3e8575e2d3bc5b85701
Yamaaada <yamadaharuka@yamadayuuka-no-MacBook-Air.local>
2013-10-24 13:13:32 +0900

    Initial Commit

---
+++ SLEquation/COINSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  COINSAppDelegate.h
+//  SLEquation
+//
+//  Created by 山田悠加 on 2013/10/24.
+//  Copyright (c) 2013年 Haruka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface COINSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SLEquation/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SLEquation
+//
+//  Created by 山田悠加 on 2013/10/24.
+//  Copyright (c) 2013年 Haruka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "COINSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([COINSAppDelegate class]));
+    }
+}
